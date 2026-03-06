2d5f7344322346900f2ac4e5bf01b8c7bde5235a
Sahib2306 <lpu.sahib@gmail.com>
2026-03-06 11:32:55 +0530

    .

--- app/utils.py
+++ app/utils.py
@@ -1,48 +1,54 @@
 import joblib
-import requests
 import numpy as np
-import re
+import requests
+import os
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-# ===== V1 MODEL =====
+# -------- LOAD V1 MODEL --------
 v1_model = joblib.load("models/v1/sentiment_model_v1.pkl")
 
-def predict_v1(text: str):
-    return v1_model.predict([text])[0]
+
+def predict_v1(text):
+    prediction = v1_model.predict([text])[0]
+    return prediction
 
 
-# ===== V2 CONFIG =====
+# -------- LOAD TOKENIZER --------
 tokenizer = joblib.load("models/v2/tokenizer_v2.pkl")
+
+# -------- LOAD LABEL ENCODER --------
 label_encoder = joblib.load("models/v2/label_encoder_v2.pkl")
 
-MAX_LEN = 100
-TF_SERVING_URL = "http://tfserving:8501/v1/models/sentiment_v2:predict"
 
-def clean_text(text):
-    text = text.lower()
-    text = re.sub(r"http\S+", "", text)
-    text = re.sub(r"[^a-zA-Z\s]", "", text)
-    text = re.sub(r"\s+", " ", text).strip()
-    return text
+# -------- TF SERVING URL --------
+TF_SERVING_URL = os.getenv(
+    "TF_SERVING_URL",
+    "http://tfserving:8501/v1/models/sentiment_v2:predict",
+)
+def predict_v2(text):
 
-
-def predict_v2(text: str):
-    # Clean
-    text = clean_text(text)
-
-    # Tokenize
+    # convert text → sequence
     sequence = tokenizer.texts_to_sequences([text])
-    padded = pad_sequences(sequence, maxlen=MAX_LEN, padding="post")
 
-    # Prepare request
+    # Must match model training input length (max_len=100 in train_v2.py)
+    padded = pad_sequences(sequence, maxlen=100, padding="post")
+
     data = {
         "instances": padded.tolist()
     }
 
     response = requests.post(TF_SERVING_URL, json=data)
-    prediction = response.json()["predictions"][0]
 
-    predicted_index = np.argmax(prediction)
-    label = label_encoder.inverse_transform([predicted_index])[0]
+    # print tensorflow response for debugging
+    print("TF RESPONSE:", response.text)
 
-    return label
+    if response.status_code != 200:
+        raise Exception(response.text)
+
+    prediction = np.array(response.json()["predictions"])
+
+    predicted_class = np.argmax(prediction, axis=1)
+
+    label = label_encoder.inverse_transform(predicted_class)
+
+    return label[0]
